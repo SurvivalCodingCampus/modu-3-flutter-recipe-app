0fd3110aa97ab12209aa435e18bca2f20276c5e9
helljh <junho308917@naver.com>
2025-04-09 16:47:13 +0900

    :sparkles: Feat: 각 버튼에 터치 기능 추가

--- lib/presentation/component/small_button.dart
+++ lib/presentation/component/small_button.dart
@@ -3,28 +3,31 @@ import 'package:recipe_app/ui/ui.dart';
 
 class SmallButton extends StatelessWidget {
   final String text;
-
-  const SmallButton({super.key, required this.text});
+  final VoidCallback? onTap;
+  const SmallButton({super.key, required this.text, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 174,
-      height: 37,
-      decoration: BoxDecoration(
-        color: ColorStyles.primary100,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            text,
-            style: TextStyles.smallerTextBold.copyWith(
-              color: ColorStyles.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 174,
+        height: 37,
+        decoration: BoxDecoration(
+          color: ColorStyles.primary100,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyles.smallerTextBold.copyWith(
+                color: ColorStyles.white,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
