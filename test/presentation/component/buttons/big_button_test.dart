c1ef752b39d1ed88af96a619810923f3a079aa62
Junsung Choi <choiexe1@gmail.com>
2025-04-09 18:18:18 +0900

    test: buttons widget test

---
+++ test/presentation/component/buttons/big_button_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:recipe_app/presentation/component/components.dart'
+    show BigButton;
+
+void main() {
+  testWidgets('BigButton 렌더링이 정상적으로 이루어져야한다.', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      BigButtonTest( text: 'Render', onPressed: () {})
+    );
+
+    Finder result = find.text('Render');
+    expect(result, findsOneWidget);
+  });
+
+  testWidgets('BigButton 클릭 시 카운트가 증가해야한다.', (WidgetTester tester) async {
+    int count = 0;
+
+    await tester.pumpWidget(
+      BigButtonTest( text: 'Render', onPressed: () => count++)
+    );
+
+    await tester.tap(find.byType(BigButton));
+    expect(count, 1);
+  });
+}
+
+class BigButtonTest extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const BigButtonTest({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BigButton(
+        text: text,
+        onPressed: () => onPressed()
+      ),
+    );
+  }
+}
---
+++ test/presentation/component/buttons/medium_button_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:recipe_app/presentation/component/components.dart'
+    show MediumButton;
+
+void main() {
+  testWidgets('MediumButton 렌더링이 정상적으로 이루어져야한다.', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MediumButtonTest(text: 'Render', onPressed: () {})
+    );
+
+    Finder result = find.text('Render');
+    expect(result, findsOneWidget);
+  });
+
+  testWidgets('MediumButton 클릭 시 카운트가 증가해야한다.', (WidgetTester tester) async {
+    int count = 0;
+
+    await tester.pumpWidget(
+      MediumButtonTest(text: 'Render', onPressed: () => count++)
+    );
+
+    await tester.tap(find.byType(MediumButton));
+    expect(count, 1);
+  });
+}
+
+class MediumButtonTest extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const MediumButtonTest({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MediumButton(
+        text: text,
+        onPressed: () => onPressed()
+      ),
+    );
+  }
+}
