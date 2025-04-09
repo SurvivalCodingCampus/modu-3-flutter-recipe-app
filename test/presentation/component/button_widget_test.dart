67cd4378702c8e7922bd52ff2c710df83ff41ed6
irmu98 <choiroom98@naver.com>
2025-04-09 16:20:20 +0900

    test: ButtonWidget 테스트 추가

---
+++ test/presentation/component/button_widget_test.dart
@@ -0,0 +1,34 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:recipe_app/presentation/component/button_widget.dart';
+
+void main() {
+  testWidgets('Button Widget onClick 테스트', (WidgetTester tester) async {
+    bool isClicked = false;
+    await tester.pumpWidget(
+        MaterialApp(
+          home: ButtonWidget(
+              buttonSize: ButtonSize.big,
+              buttonText: 'Button',
+              onClick: () {
+                isClicked = !isClicked;
+              }
+          ),
+        )
+    );
+    final Finder bigButtonFinder = find.text('Button');
+    expect(bigButtonFinder, findsOneWidget);
+
+    await tester.tap(bigButtonFinder);
+    await tester.pump();
+    expect(isClicked, isTrue);
+  });
+}
