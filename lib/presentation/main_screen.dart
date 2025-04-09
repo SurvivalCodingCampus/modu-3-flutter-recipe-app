92714aea5f57d1b2dee4c9d31d09d43c17f214d8
irmu98 <choiroom98@naver.com>
2025-04-09 17:39:59 +0900

    test: unfocus 추가

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -9,48 +9,53 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('과제중...'), centerTitle: true),
-      body: Center(
-        child: Column(
-          children: [
-            ButtonWidget(
-              buttonSize: ButtonSize.big,
-              buttonText: 'Button',
-              onClick: () {
-                print('Big Button');
-              },
-            ),
-            ButtonWidget(
-              buttonSize: ButtonSize.medium,
-              buttonText: 'Button',
-              onClick: () {
-                print('Medium Button');
-              },
-            ),
-            ButtonWidget(
-              buttonSize: ButtonSize.small,
-              buttonText: 'Button',
-              onClick: () {
-                print('Small Button');
-              },
-            ),
-            InputFieldWidget(
-              label: 'Label',
-              placeHolder: 'PlaceHolder',
-              value: 'PlaceHolder',
-              onValueChange: (String value) {
-                print(value);
-              },
-            ),
-            TabsWidget(
-              onValueChange: (index) {
-                print('index가 $index로 바뀌었습니다.');
-              },
-              labels: ['Label0', 'Label1'],
-              selectedIndex: 0,
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('과제중...'), centerTitle: true),
+        body: Center(
+          child: Column(
+            children: [
+              ButtonWidget(
+                buttonSize: ButtonSize.big,
+                buttonText: 'Button',
+                onClick: () {
+                  print('Big Button');
+                },
+              ),
+              ButtonWidget(
+                buttonSize: ButtonSize.medium,
+                buttonText: 'Button',
+                onClick: () {
+                  print('Medium Button');
+                },
+              ),
+              ButtonWidget(
+                buttonSize: ButtonSize.small,
+                buttonText: 'Button',
+                onClick: () {
+                  print('Small Button');
+                },
+              ),
+              InputFieldWidget(
+                label: 'Label',
+                placeHolder: 'PlaceHolder',
+                value: 'PlaceHolder',
+                onValueChange: (String value) {
+                  print(value);
+                },
+              ),
+              TabsWidget(
+                onValueChange: (index) {
+                  print('index가 $index로 바뀌었습니다.');
+                },
+                labels: ['Label0', 'Label1'],
+                selectedIndex: 0,
+              ),
+            ],
+          ),
         ),
       ),
     );
