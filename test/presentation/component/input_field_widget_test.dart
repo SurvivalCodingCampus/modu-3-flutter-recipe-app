88b7095e6f555e60b03d07d54fc3dd8ba97db047
irmu98 <choiroom98@naver.com>
2025-04-09 17:10:29 +0900

    test: label, placeHolder 확인 테스트 추가

---
+++ test/presentation/component/input_field_widget_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:recipe_app/presentation/component/input_field_widget.dart';
+
+void main() {
+  testWidgets('InputFieldWidget onClick 테스트', (WidgetTester tester) async {
+    String sampleText = '';
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: InputFieldWidget(
+            label: 'label',
+            placeHolder: 'placeHolder',
+            value: 'value',
+            onValueChange: (text) {
+              sampleText = text;
+            },
+          ),
+        ),
+      ),
+    );
+
+    final Finder textField = find.byKey(const Key('textField'));
+    final Finder label = find.text('label');
+    final Finder placeHolder = find.text('placeHolder');
+    expect(textField, findsOneWidget);
+    expect(label, findsOneWidget);
+    expect(placeHolder, findsOneWidget);
+
+    await tester.enterText(textField, '12345');
+    expect(sampleText, '12345');
+  });
+}
