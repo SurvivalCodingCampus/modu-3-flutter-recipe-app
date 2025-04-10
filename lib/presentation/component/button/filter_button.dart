47b5212ed3f609ac91aabefcc27ed55b60fe76d9
Si Yujun <siuj0810@gmail.com>
2025-04-10 19:01:54 +0900

    design: filter button component 추가 완료

---
+++ lib/presentation/component/button/filter_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/ui/color_style.dart';
+import 'package:recipe_app/ui/text_font_style.dart';
+
+class FilterButton extends StatelessWidget {
+  final String text;
+  final bool isSelected;
+  final bool starIcon;
+  final VoidCallback onChanged;
+
+  const FilterButton({
+    super.key,
+    required this.text,
+    required this.isSelected,
+    required this.onChanged,
+    required this.starIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onChanged,
+      child: IntrinsicWidth(
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: ColorStyle.primary80),
+            borderRadius: BorderRadius.circular(10),
+            color: isSelected ? ColorStyle.primary100 : ColorStyle.white,
+          ),
+          curve: Curves.easeInOut,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Row(
+              children: [
+                Text(
+                  text,
+                  style: TextFontStyle.smallRegular(
+                    color:
+                        isSelected ? ColorStyle.white : ColorStyle.primary100,
+                  ),
+                ),
+                starIcon
+                    ? Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Icon(
+                        Icons.star,
+                        color:
+                            isSelected
+                                ? ColorStyle.white
+                                : ColorStyle.primary100,
+                      ),
+                    )
+                    : SizedBox.shrink(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/home_view.dart
+++ lib/presentation/home_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/model/ingredient.dart';
-import 'package:recipe_app/model/recipe.dart';
 
-import 'package:recipe_app/presentation/component/recipe_card.dart';
+import 'package:recipe_app/presentation/component/button/filter_button.dart';
+import 'package:recipe_app/ui/text_font_style.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,66 +11,109 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  List<Ingredient> items = [
-    Ingredient(
-      id: '1',
-      name: 'Tomato',
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBGQA-90wUSZLTbDOZRx82dfgbbWIR-JPFuw&s',
-      weight: 500,
-    ),
-    Ingredient(
-      id: '2',
-      name: 'Cabbage',
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0isXIBXi9gOEeS7YJ_BgHUijYl3na5K8PbQ&s',
-      weight: 400,
-    ),
-    Ingredient(
-      id: '3',
-      name: 'Taco',
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpQgdor4G2jrYMXlvISP00BL_BInU43jOblw&s',
-      weight: 300,
-    ),
+  List<String> stars = ['5', '4', '3', '2', '1'];
+  List<String> filters = ['All', 'Newest', 'Oldest', 'Popularity'];
+  List<String> categorys = [
+    'All',
+    'Cereal',
+    'Vegetables',
+    'Dinner',
+    'Chinese',
+    'Local Dish',
+    'Fruit',
+    'BreakFast',
+    'Spanish',
+    'Lunch',
   ];
-
-  List<Recipe> recipes = [
-    Recipe(
-      id: '1',
-      title: 'Taco',
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmZw6XPQ46etY0sTf13hZW3Gi3z_63c-aZHQ&s',
-      rate: 2.0,
-      cookingTime: 15,
-    ),
-    Recipe(
-      id: '2',
-      title: 'Hamberger is very nice',
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSJ94wG3vijhGRXiIa_Z__AvEnvpsdOtsQ_A&s',
-      rate: 3.5,
-      cookingTime: 5,
-    ),
-    Recipe(
-      id: '3',
-      title: 'Pizza',
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaxVhAJD85NE9WNvuxfmutOJapD3EqHAuenQ&s',
-      rate: 4.5,
-      cookingTime: 20,
-    ),
-  ];
-
+  Set<String> seleted = {};
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (context, index) {
-          final recipe = recipes[index];
-          return RecipeCard(recipe: recipe);
-        },
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Time', style: TextFontStyle.smallBold()),
+            SizedBox(height: 10),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(filters.length, (index) {
+                final filter = filters[index];
+                return FilterButton(
+                  text: filter,
+                  isSelected: seleted.contains(filter),
+                  onChanged: () {
+                    setState(() {
+                      if (seleted.contains(filter)) {
+                        seleted.remove(filter);
+                        print('지우기');
+                      } else {
+                        seleted.add(filter);
+                        print('추가');
+                      }
+                    });
+                  },
+                  starIcon: false,
+                );
+              }),
+            ),
+            SizedBox(height: 10),
+            Text('Rate', style: TextFontStyle.smallBold()),
+            SizedBox(height: 10),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(stars.length, (index) {
+                final star = stars[index];
+                return FilterButton(
+                  text: star,
+                  isSelected: seleted.contains(star),
+                  onChanged: () {
+                    setState(() {
+                      if (seleted.contains(star)) {
+                        seleted.remove(star);
+                        print('지우기');
+                      } else {
+                        seleted.add(star);
+                        print('추가');
+                      }
+                    });
+                  },
+                  starIcon: true,
+                );
+              }),
+            ),
+            SizedBox(height: 10),
+            Text('Category', style: TextFontStyle.smallBold()),
+            SizedBox(height: 10),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(categorys.length, (index) {
+                final category = categorys[index];
+                return FilterButton(
+                  text: category,
+                  isSelected: seleted.contains(category),
+                  onChanged: () {
+                    setState(() {
+                      if (seleted.contains(category)) {
+                        seleted.remove(category);
+                        print('지우기');
+                      } else {
+                        seleted.add(category);
+                        print('추가');
+                      }
+                    });
+                  },
+                  starIcon: false,
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
