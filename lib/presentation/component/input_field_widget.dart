86581217ada5e3f375986ad05fddb7346b834ade
irmu98 <choiroom98@naver.com>
2025-04-10 13:47:19 +0900

    fix: 내부에 사이즈를 직접 지정하지 않도록 수정

--- lib/presentation/component/input_field_widget.dart
+++ lib/presentation/component/input_field_widget.dart
@@ -19,50 +19,34 @@ class InputFieldWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController controller = TextEditingController(text: value);
-    return Container(
-      width: 315,
-      height: 81,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 50,
-                height: 21,
-                child: Text(
-                  label,
-                  style: TextStyles.smallRegular(color: ColorStyles.labelColor),
-                ),
-              ),
-            ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 4.0),
+          child: Text(
+            label,
+            style: TextStyles.smallRegular(color: ColorStyles.labelColor),
           ),
-          Container(
-            width: 315,
-            height: 55,
-            child: TextField(
-              key: const Key('textField'),
-              controller: controller,
-              onChanged: (text) {
-                onValueChange(controller.text);
-              },
-              decoration: InputDecoration(
-                hintText: placeHolder,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: ColorStyles.gray4, width: 1.5),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: ColorStyles.primary80,
-                    width: 1.5,
-                  ),
-                ),
-              ),
+        ),
+        TextField(
+          key: const Key('textField'),
+          controller: controller,
+          onChanged: (text) => onValueChange(text),
+          decoration: InputDecoration(
+            hintText: placeHolder,
+            contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: ColorStyles.gray4, width: 1.5),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: ColorStyles.primary80, width: 1.5),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
