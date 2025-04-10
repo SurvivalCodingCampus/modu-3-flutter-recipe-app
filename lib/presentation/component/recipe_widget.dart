2580a2827320f06fbd3a671fdf5fae2c8a4779dd
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-04-10 11:51:33 +0900

    feat: add StatefulWidget rule and Tabs component
    - Change RecipeWidget to StatefulWidget to handle background color changes on tap.
    - Add Tabs component for displaying selectable tabs with visual feedback.
    - Create TabsTestScreen to demonstrate the Tabs component functionality.

--- lib/presentation/component/recipe_widget.dart
+++ lib/presentation/component/recipe_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../data/model/recipe.dart';
 
-class RecipeWidget extends StatelessWidget {
+class RecipeWidget extends StatefulWidget {
   final void Function(String name)? onTapName;
   final void Function(Recipe recipe)? onTapImageUrl;
   final void Function(Recipe recipe)? onTap;
@@ -18,15 +18,30 @@ class RecipeWidget extends StatelessWidget {
   });
 
   @override
+  State<RecipeWidget> createState() => _RecipeWidgetState();
+}
+
+class _RecipeWidgetState extends State<RecipeWidget> {
+  Color _backgroundColor = Colors.blue;
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: GestureDetector(
+        onTapDown: (details) {
+          setState(() {
+            _backgroundColor = Colors.yellow;
+          });
+        },
         onTap: () {
-          onTap?.call(recipe);
+          setState(() {
+            _backgroundColor = Colors.blue;
+          });
+          widget.onTap?.call(widget.recipe);
         },
         child: Container(
           decoration: BoxDecoration(
-            color: Colors.blue,
+            color: _backgroundColor,
             borderRadius: BorderRadius.circular(10),
           ),
           child: Column(
@@ -35,13 +50,13 @@ class RecipeWidget extends StatelessWidget {
             children: [
               GestureDetector(
                 onTap: () {
-                  onTapName?.call(recipe.name);
+                  widget.onTapName?.call(widget.recipe.name);
                 },
                 child: Row(
                   children: [
                     Icon(Icons.arrow_forward),
                     Text(
-                      recipe.name,
+                      widget.recipe.name,
                       style: TextStyle(fontSize: 40),
                       key: const Key('name'),
                     ),
@@ -50,9 +65,9 @@ class RecipeWidget extends StatelessWidget {
               ),
               GestureDetector(
                 onTap: () {
-                  onTapImageUrl?.call(recipe);
+                  widget.onTapImageUrl?.call(widget.recipe);
                 },
-                child: Text(recipe.imageUrl, style: TextStyle(fontSize: 40)),
+                child: Text(widget.recipe.imageUrl, style: TextStyle(fontSize: 40)),
               ),
             ],
           ),
