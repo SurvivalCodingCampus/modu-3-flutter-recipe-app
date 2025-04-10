e290bbaf0ff55e45788013f80d1f26fa722465c3
Si Yujun <siuj0810@gmail.com>
2025-04-10 16:05:41 +0900

    design: ingredient item, recipe card component 구현 완료, 간단한 model 작업 완료

---
+++ lib/presentation/component/ingredient_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/model/ingredient.dart';
+import 'package:recipe_app/ui/color_style.dart';
+import 'package:recipe_app/ui/text_font_style.dart';
+
+class IngredientItem extends StatelessWidget {
+  const IngredientItem({super.key, required this.item});
+
+  final Ingredient item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: ColorStyle.gray4,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 12),
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+      width: double.infinity,
+      child: ListTile(
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: SizedBox(
+            width: 52,
+            height: 52,
+            child: Image.network(item.imageUrl, fit: BoxFit.cover),
+          ),
+        ),
+        title: Text(item.name, style: TextFontStyle.normalBold()),
+        trailing: Text(
+          '${item.weight.toString()}g',
+          style: TextFontStyle.smallRegular(color: ColorStyle.gray3),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/home_view.dart
+++ lib/presentation/home_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/model/ingredient.dart';
+import 'package:recipe_app/model/recipe.dart';
 
-import 'package:recipe_app/presentation/component/input_field.dart';
-
-import 'package:recipe_app/presentation/component/tabs.dart';
+import 'package:recipe_app/presentation/component/recipe_card.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,32 +12,66 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int seletedIndex = 0;
+  List<Ingredient> items = [
+    Ingredient(
+      id: '1',
+      name: 'Tomato',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBGQA-90wUSZLTbDOZRx82dfgbbWIR-JPFuw&s',
+      weight: 500,
+    ),
+    Ingredient(
+      id: '2',
+      name: 'Cabbage',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0isXIBXi9gOEeS7YJ_BgHUijYl3na5K8PbQ&s',
+      weight: 400,
+    ),
+    Ingredient(
+      id: '3',
+      name: 'Taco',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpQgdor4G2jrYMXlvISP00BL_BInU43jOblw&s',
+      weight: 300,
+    ),
+  ];
+
+  List<Recipe> recipes = [
+    Recipe(
+      id: '1',
+      title: 'Taco',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmZw6XPQ46etY0sTf13hZW3Gi3z_63c-aZHQ&s',
+      rate: 2.0,
+      cookingTime: 15,
+    ),
+    Recipe(
+      id: '2',
+      title: 'Hamberger is very nice',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSJ94wG3vijhGRXiIa_Z__AvEnvpsdOtsQ_A&s',
+      rate: 3.5,
+      cookingTime: 5,
+    ),
+    Recipe(
+      id: '3',
+      title: 'Pizza',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaxVhAJD85NE9WNvuxfmutOJapD3EqHAuenQ&s',
+      rate: 4.5,
+      cookingTime: 20,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        spacing: 20,
-        children: [
-          InputField(
-            label: 'label',
-            placeholder: 'placeholder',
-            onValueChange: (value) {
-              print(value);
-            },
-          ),
-          Tabs(
-            labels: ['label1', 'label2'],
-            onValueChange: (index) {
-              setState(() {
-                seletedIndex = index;
-              });
-            },
-            selectedIndex: seletedIndex,
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: recipes.length,
+        itemBuilder: (context, index) {
+          final recipe = recipes[index];
+          return RecipeCard(recipe: recipe);
+        },
       ),
     );
   }
