fc37ac86c9130fac8973e7a08b1bf89237d4bd69
Si Yujun <siuj0810@gmail.com>
2025-04-10 21:14:21 +0900

    fix: button state 에 따른 변화 추가 (tap down, tap up, tap cancel)

--- lib/presentation/component/button/small_button.dart
+++ lib/presentation/component/button/small_button.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/presentation/component/button/enum/button_type.dart';
 import 'package:recipe_app/ui/color_style.dart';
 import 'package:recipe_app/ui/text_font_style.dart';
 
-class SmallButton extends StatelessWidget {
+class SmallButton extends StatefulWidget {
   final String text;
   final VoidCallback onTap;
 
   const SmallButton({super.key, required this.text, required this.onTap});
 
   @override
+  State<SmallButton> createState() => _SmallButtonState();
+}
+
+class _SmallButtonState extends State<SmallButton> {
+  var buttonType = ButtonType.standard;
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        onTap();
+      onTap: widget.onTap,
+      onTapDown: (details) {
+        setState(() {
+          buttonType = ButtonType.disabled;
+        });
       },
-      child: Container(
+      onTapUp: (details) {
+        setState(() {
+          buttonType = ButtonType.standard;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          buttonType = ButtonType.standard;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
         width: 174,
         decoration: BoxDecoration(
-          color: ColorStyle.primary100,
+          color:
+              buttonType == ButtonType.standard
+                  ? ColorStyle.primary100
+                  : ColorStyle.gray4,
           borderRadius: BorderRadius.circular(10),
         ),
         child: Padding(
@@ -28,7 +52,7 @@ class SmallButton extends StatelessWidget {
               SizedBox(
                 width: 114,
                 child: Text(
-                  text,
+                  widget.text,
                   style: TextFontStyle.smallBold(color: ColorStyle.white),
                   textAlign: TextAlign.center,
                 ),
--- lib/presentation/home_view.dart
+++ lib/presentation/home_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'package:recipe_app/presentation/component/button/filter_button.dart';
-import 'package:recipe_app/ui/text_font_style.dart';
+import 'package:recipe_app/presentation/component/button/big_button.dart';
+import 'package:recipe_app/presentation/component/button/enum/button_type.dart';
+import 'package:recipe_app/presentation/component/button/medium_button.dart';
+import 'package:recipe_app/presentation/component/button/small_button.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -26,94 +27,34 @@ class _HomeViewState extends State<HomeView> {
     'Lunch',
   ];
   Set<String> seleted = {};
+  ButtonType buttonType = ButtonType.standard;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Time', style: TextFontStyle.smallBold()),
-            SizedBox(height: 10),
-            Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: List.generate(filters.length, (index) {
-                final filter = filters[index];
-                return FilterButton(
-                  text: filter,
-                  isSelected: seleted.contains(filter),
-                  onChanged: () {
-                    setState(() {
-                      if (seleted.contains(filter)) {
-                        seleted.remove(filter);
-                        print('지우기');
-                      } else {
-                        seleted.add(filter);
-                        print('추가');
-                      }
-                    });
-                  },
-                  starIcon: false,
-                );
-              }),
-            ),
-            SizedBox(height: 10),
-            Text('Rate', style: TextFontStyle.smallBold()),
-            SizedBox(height: 10),
-            Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: List.generate(stars.length, (index) {
-                final star = stars[index];
-                return FilterButton(
-                  text: star,
-                  isSelected: seleted.contains(star),
-                  onChanged: () {
-                    setState(() {
-                      if (seleted.contains(star)) {
-                        seleted.remove(star);
-                        print('지우기');
-                      } else {
-                        seleted.add(star);
-                        print('추가');
-                      }
-                    });
-                  },
-                  starIcon: true,
-                );
-              }),
-            ),
-            SizedBox(height: 10),
-            Text('Category', style: TextFontStyle.smallBold()),
-            SizedBox(height: 10),
-            Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: List.generate(categorys.length, (index) {
-                final category = categorys[index];
-                return FilterButton(
-                  text: category,
-                  isSelected: seleted.contains(category),
-                  onChanged: () {
-                    setState(() {
-                      if (seleted.contains(category)) {
-                        seleted.remove(category);
-                        print('지우기');
-                      } else {
-                        seleted.add(category);
-                        print('추가');
-                      }
-                    });
-                  },
-                  starIcon: false,
-                );
-              }),
-            ),
-          ],
-        ),
+      body: Column(
+        spacing: 20,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigButton(
+            text: 'Button',
+            onTap: () {
+              print('버튼');
+            },
+          ),
+          MediumButton(
+            text: 'Button',
+            onTap: () {
+              print('버튼');
+            },
+          ),
+          SmallButton(
+            text: 'Button',
+            onTap: () {
+              print('버튼');
+            },
+          ),
+        ],
       ),
     );
   }
