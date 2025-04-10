d83c9b5ad1e73111224d17130c0c833f694c504f
KangJiWon <doggodking@gmail.com>
2025-04-10 20:48:07 +0900

    fix : 텍스트 필드 오류 수정

--- lib/presentation/component/input_field.dart
+++ lib/presentation/component/input_field.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app/ui/color_style.dart';
 import 'package:recipe_app/ui/text_style.dart';
 
-class InputField extends StatelessWidget {
+class InputField extends StatefulWidget {
   final String labelTitle;
   final String placeholderText;
   final String value;
-
   final void Function(String)? onValueChange;
 
   const InputField({
@@ -18,55 +17,85 @@ class InputField extends StatelessWidget {
   });
 
   @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.value);
+  }
+
+  @override
+  void didUpdateWidget(covariant InputField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.value != widget.value && _controller.text != widget.value) {
+      _controller.text = widget.value;
+      _controller.selection = TextSelection.fromPosition(
+        TextPosition(offset: widget.value.length),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final double rounded = 10.0;
-    const double placeholderVerticalPadding = 20.0;
-    const double placeholderHorizontalPadding = 20.0;
+    const double borderRadius = 10.0;
     const double borderWidth = 1.5;
+    const double fieldHeight = 55.0;
+    const double verticalPadding = 20.0;
+    const double horizontalPadding = 20.0;
+    const double labelSpacing = 8.0;
 
     return Column(
       children: [
         Align(
           alignment: Alignment.centerLeft,
           child: Text(
-            labelTitle,
+            widget.labelTitle,
             style: AppTextStyles.smallRegular(color: ColorStyle.labelColour),
           ),
         ),
-        const SizedBox(height: 8),
+        const SizedBox(height: labelSpacing),
         SizedBox(
-          height: 55,
+          height: fieldHeight,
           child: TextField(
-            controller: TextEditingController(text: value),
-            onChanged: (value) {
-              onValueChange?.call(value);
-            },
+            controller: _controller,
+            onChanged: widget.onValueChange,
             style: AppTextStyles.extraSmallRegular(color: ColorStyle.black),
             decoration: InputDecoration(
-              hintText: placeholderText,
+              hintText: widget.placeholderText,
               hintStyle: AppTextStyles.smallRegular(color: ColorStyle.gray4),
               filled: true,
               fillColor: ColorStyle.white,
               contentPadding: const EdgeInsets.symmetric(
-                vertical: placeholderVerticalPadding,
-                horizontal: placeholderHorizontalPadding,
+                vertical: verticalPadding,
+                horizontal: horizontalPadding,
               ),
               border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(rounded),
+                borderRadius: BorderRadius.circular(borderRadius),
                 borderSide: const BorderSide(
                   color: ColorStyle.gray4,
                   width: borderWidth,
                 ),
               ),
               focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(rounded),
+                borderRadius: BorderRadius.circular(borderRadius),
                 borderSide: const BorderSide(
                   color: ColorStyle.primary80,
                   width: borderWidth,
                 ),
               ),
               enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(rounded),
+                borderRadius: BorderRadius.circular(borderRadius),
                 borderSide: const BorderSide(
                   color: ColorStyle.gray4,
                   width: borderWidth,
