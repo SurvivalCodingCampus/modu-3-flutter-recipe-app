ff639b738266e813000b0184ad55ec8322c8721b
helljh <junho308917@naver.com>
2025-04-10 12:58:40 +0900

    :sparkles: Feat: IngredientItem 위젯 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/domain/model/model.dart';
 import 'package:recipe_app/presentation/component/big_button.dart';
 import 'package:recipe_app/presentation/component/label_input_field.dart';
 import 'package:recipe_app/presentation/component/medium_button.dart';
 import 'package:recipe_app/presentation/component/small_button.dart';
 import 'package:recipe_app/presentation/component/tabs.dart';
+import 'package:recipe_app/ui/ui.dart';
+
+import 'presentation/component/ingrident_item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
       home: const ComponentsWidget(),
@@ -31,47 +36,61 @@ class ComponentsWidget extends StatelessWidget {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
+        backgroundColor: ColorStyles.white,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                BigButton(
-                  text: 'Button',
-                  onTap: () {
-                    print('Big Button Pressed');
-                  },
-                ),
-                const SizedBox(height: 10),
-                MediumButton(
-                  text: 'Button',
-                  onTap: () {
-                    print('Medium Button Pressed');
-                  },
-                ),
-                const SizedBox(height: 10),
-                SmallButton(
-                  text: 'Button',
-                  onTap: () {
-                    print('Small Button Pressed');
-                  },
-                ),
-                const SizedBox(height: 10),
-                LabelInputField(
-                  label: 'label',
-                  hintText: 'hintText',
-                  onValueChange: (String value) {
-                    print(value);
-                  },
-                ),
-                const SizedBox(height: 10),
-                Tabs(
-                  labels: const ['one', 'two', 'three'],
-                  selectedIndex: 0,
-                  onTap: (int index) {
-                    print(index);
-                  },
-                ),
-              ],
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                children: [
+                  BigButton(
+                    text: 'Button',
+                    onTap: () {
+                      print('Big Button Pressed');
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  MediumButton(
+                    text: 'Button',
+                    onTap: () {
+                      print('Medium Button Pressed');
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  SmallButton(
+                    text: 'Button',
+                    onTap: () {
+                      print('Small Button Pressed');
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  LabelInputField(
+                    label: 'label',
+                    hintText: 'hintText',
+                    onValueChange: (String value) {
+                      print(value);
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  Tabs(
+                    labels: const ['one', 'two', 'three'],
+                    selectedIndex: 0,
+                    onTap: (int index) {
+                      print(index);
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  const IngridentItem(
+                    ingredient: Ingredient(
+                      id: '1',
+                      name: 'Tomato',
+                      imageUrl:
+                          'https://s3-alpha-sig.figma.com/img/3c30/4e61/3dd3e1dd1f7360a6ef1c04399b0609ab?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Z1o~faIwyt6-juKGeztJ2D8GDEM66ccRS8CMg0e4-Ldw9tXRosaROZFDCQrLROVxG9RuaxwLP5ZjfgM7fRROYE4OVoiEQWdPjxN~9cWWWzFiG79eK-ZeDreGZBlJ39ps1vi~gaImP6HY83~-MVt7rBouxK2LvI9y2T-dnIbJzpO3FKVDyL~BQ328ez6-5Ef40tsgLqIlb3JCcClbRpDU5sh0fd4YhRTM88ELeC1brmBVIwJukIy85zZWy88zprw78~fpQ8eu9uPHFDq8j677ng4J7fO942x19hUwK9zgxiQe9R2EDS60ZOTydWh12UYGDq6MiAz~tDjcYV0mEUQm9Q__',
+                      weight: 500,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/presentation/component/ingrident_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:recipe_app/domain/model/model.dart';
+import 'package:recipe_app/ui/ui.dart';
+
+class IngridentItem extends StatelessWidget {
+  final Ingredient ingredient;
+  const IngridentItem({super.key, required this.ingredient});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      width: double.infinity,
+      height: 76,
+      decoration: BoxDecoration(
+        color: ColorStyles.gray4,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 52,
+            height: 52,
+            decoration: BoxDecoration(
+              color: ColorStyles.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.network(ingredient.imageUrl),
+          ),
+          const SizedBox(width: 16),
+          Text(ingredient.name, style: TextStyles.normalTextBold),
+          const Spacer(),
+          Text(
+            '${ingredient.weight}g',
+            style: TextStyles.smallTextRegular.copyWith(
+              color: ColorStyles.gray3,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
