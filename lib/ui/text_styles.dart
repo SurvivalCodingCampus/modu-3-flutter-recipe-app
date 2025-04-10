6e1a6417626bbba6ef0e1f38f80729f00e2e945b
Junsung Choi <choiexe1@gmail.com>
2025-04-10 16:28:53 +0900

    feat: texy_style remove line height

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -4,91 +4,78 @@ abstract class TextStyles {
   static const TextStyle titleTextBold = TextStyle(
     fontSize: 50,
     fontWeight: FontWeight.w600,
-    height: 1.46,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle headerTextBold = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.w600,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle largeTextBold = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w600,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle mediumTextBold = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle normalTextBold = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w600,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle smallTextBold = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w600,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle titleTextRegular = TextStyle(
     fontSize: 50,
     fontWeight: FontWeight.w400,
-    height: 1.46,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle headerTextRegular = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.w400,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle largeTextRegular = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w400,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle mediumTextRegular = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w400,
-    height: 1.28,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle normalTextRegular = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
-    height: 1.31,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle labelTextBold = TextStyle(
     fontSize: 10,
     fontWeight: FontWeight.w400,
-    height: 1.5,
     fontFamily: 'Poppins',
   );
 
   static const TextStyle smallTextRegular = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
-    height: 1.64,
     fontFamily: 'Poppins',
   );
 }
