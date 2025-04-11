dcc1ed8b097fa72be4f2a7b5f95ce7a9005220bb
irmu98 <choiroom98@naver.com>
2025-04-11 17:50:34 +0900

    refactor: 단순 코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/data/data_source/mock_recipe_data_source_impl.dart';
-import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
-import 'package:recipe_app/presentation/home/home_screen.dart';
-import 'package:recipe_app/presentation/home/home_view_model.dart';
 import 'package:recipe_app/presentation/splash/splash_screen.dart';
-import 'package:recipe_app/presentation/splash/splash_view_model.dart';
 
 void main() {
   runApp(const MyApp());
