864965f19f008fbf20d73e6d55862c0a8394d1b6
irmu98 <choiroom98@naver.com>
2025-04-11 17:11:30 +0900

    fix: MockDataSource를 이용한 테스트 가능하도록 수정

--- lib/data/data_source/mock_recipe_data_source_impl.dart
+++ lib/data/data_source/mock_recipe_data_source_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:recipe_app/data/data_source/recipe_data_source.dart';
 
 import '../dto/recipe_dto.dart';
@@ -8,8 +9,8 @@ import '../dto/recipe_dto.dart';
 class MockRecipeDataSourceImpl implements RecipeDataSource{
   @override
   Future<List<RecipeDto>> getRecipeDtos() async {
-    final File file = File('assets/mock_data/recipes.json');
-    final String fileString = await file.readAsString();
+    // final File file = File('lib/data/mock_data/recipes.json');
+    final String fileString = await rootBundle.loadString('assets/mock_data/recipes.json');
     final Map<String, dynamic> json = jsonDecode(fileString);
     final List<dynamic> recipes = json['recipes'];
 
--- lib/data/repository/recipe_repository_impl.dart
+++ lib/data/repository/recipe_repository_impl.dart
@@ -1,10 +1,9 @@
 import 'package:recipe_app/data/data_source/recipe_data_source.dart';
-import 'package:recipe_app/data/data_source/recipe_data_source_impl.dart';
 import 'package:recipe_app/data/mapper/recipe_mapper.dart';
 import 'package:recipe_app/data/model/recipe.dart';
 import 'package:recipe_app/data/repository/recipe_repository.dart';
 
-import '../data_source/mock_recipe_data_source_imple.dart';
+import '../data_source/mock_recipe_data_source_impl.dart';
 import '../dto/recipe_dto.dart';
 
 class RecipeRepositoryImpl implements RecipeRepository {
