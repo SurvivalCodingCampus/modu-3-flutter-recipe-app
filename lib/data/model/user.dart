635322ab8595140dda3d29bb17612ad119869680
irmu98 <choiroom98@naver.com>
2025-04-11 17:09:14 +0900

    feat: 북마크 아이디 저장을 위한 User 클래스 수정

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -1,14 +1,18 @@
+
+// =============================== 임시 ===================================
 class User {
   final int id;
   final String name;
   final String image;
   final String address;
+  final Set<int> bookmarks;
 
   const User({
     required this.id,
     required this.name,
     required this.image,
-    required this.address
+    required this.address,
+    required this.bookmarks
   });
 
   // final String email;
@@ -17,9 +21,7 @@ class User {
   // final String profileImage;
   // final String introduce;
   // final List<int> recipes;
-  // final List<int> bookmarks;
   // final List<String> follower;
   // final List<String> following;
   // final List<int> notifications;
-
 }
