ebf6561192d6856a86863a8695c2c01d5d5a512b
KangJiWon <doggodking@gmail.com>
2025-04-11 23:05:15 +0900

    fix: RecipeRepositoryImpl 로직 개선

--- lib/data/repository/recipe_repository_impl.dart
+++ lib/data/repository/recipe_repository_impl.dart
@@ -18,23 +18,26 @@ class RecipeRepositoryImpl implements RecipeRepository {
 
   @override
   Future<Result<List<Recipe>>> getRecipes() async {
-    try {
-      final source = remoteDataSource ?? localDataSource;
-      final List<RecipeDto> recipeDtos = await source.fetchRecipes();
-      final List<Recipe> models = recipeDtos.map((e) => e.toRecipe()).toList();
-      return Result.success(models);
-    } catch (e, _) {
-      debugPrint('⚠️ remoteDataSource 실패 → local fallback');
-
+    // 원격 데이터 소스가 있는 경우 먼저 시도
+    if (remoteDataSource != null) {
       try {
-        final List<RecipeDto> recipeDtos = await localDataSource.fetchRecipes();
+        final List<RecipeDto> recipeDtos =
+            await remoteDataSource!.fetchRecipes();
         final List<Recipe> models =
             recipeDtos.map((e) => e.toRecipe()).toList();
         return Result.success(models);
-      } catch (e2) {
-        debugPrint('⚠️ localDataSource 실패 → error fallback $e2)');
-        return Result.error(Failure('레시피 데이터를 불러오는 데 실패했습니다.', cause: e2));
+      } catch (e, _) {
+        debugPrint('⚠️ remoteDataSource 실패 → local fallback');
       }
+    }
+    // 원격 데이터 소스가 없거나 실패한 경우 로컬 데이터 소스 사용
+    try {
+      final List<RecipeDto> recipeDtos = await localDataSource.fetchRecipes();
+      final List<Recipe> models = recipeDtos.map((e) => e.toRecipe()).toList();
+      return Result.success(models);
+    } catch (e2) {
+      debugPrint('⚠️ localDataSource 실패 → error fallback $e2)');
+      return Result.error(Failure('레시피 데이터를 불러오는 데 실패했습니다.', cause: e2));
     }
   }
 }
