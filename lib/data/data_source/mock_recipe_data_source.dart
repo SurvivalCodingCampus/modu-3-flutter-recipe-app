e9ebdbe23483e19bdcda608d67b948c1adb24fc8
eastar-DS <sien0104@gmail.com>
2025-04-11 13:46:51 +0900

    feat: DataSource 인터페이스 정의 및 MockDataSource 생성

---
+++ lib/data/data_source/mock_recipe_data_source.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:recipe_app/data/data_source/recipe_data_source.dart';
+import 'package:http/http.dart' as http;
+
+class MockRecipeDataSource implements RecipeDataSource {
+  final http.Client _client;
+  final String _url;
+
+  MockRecipeDataSource({required http.Client client, required String url})
+    : _client = client,
+      _url = url;
+
+  @override
+  Future<List<Map<String, dynamic>>> getRecipeMaps() async {
+    String jsonString =
+        await File(
+          '${Directory.current.path}/lib/data/json/recipes.json',
+        ).readAsString();
+    Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+    List recipeList = jsonMap["recipes"];
+    final List<Map<String, dynamic>> result =
+        recipeList.map((e) {
+          String category = e["category"];
+          e["category"] = category.toLowerCase();
+          return e as Map<String, dynamic>;
+        }).toList();
+    return result;
+  }
+}
