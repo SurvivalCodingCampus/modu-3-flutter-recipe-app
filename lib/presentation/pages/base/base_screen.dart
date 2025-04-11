1ac96aba83d453b31eb17f6bd0b30c4c9a1d5b12
Dansot4891 <cnctlim94@gmail.com>
2025-04-11 12:37:55 +0900

    [feat] 대부분 화면의 base가 되는 screen 구현

---
+++ lib/presentation/pages/base/base_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  final Widget child;
+  final Color? bgColor;
+  final Widget? bottomNavigationBar;
+  final PreferredSizeWidget? appBar;
+  final bool resizeToAvoidBottomInset;
+  final FloatingActionButton? floatingActionButton;
+  const BaseScreen({
+    required this.child,
+    this.bgColor = Colors.white,
+    this.bottomNavigationBar,
+    this.resizeToAvoidBottomInset = false,
+    this.appBar,
+    this.floatingActionButton,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // 텍스트 필드에서 바깥 영역 터치시 해제가 되도록 설정
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+        appBar: appBar,
+        backgroundColor: bgColor,
+        body: child,
+        bottomNavigationBar: bottomNavigationBar,
+        floatingActionButton: floatingActionButton,
+      ),
+    );
+  }
+}
