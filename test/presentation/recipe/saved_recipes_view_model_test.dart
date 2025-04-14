1efe88989c880f58de4e899c7c7440d752734327
Junsung Choi <choiexe1@gmail.com>
2025-04-14 13:32:00 +0900

    fix: test fix

--- test/presentation/recipe/saved_recipes_view_model_test.dart
+++ test/presentation/recipe/saved_recipes_view_model_test.dart
@@ -3,7 +3,7 @@ import 'package:recipe_app/data/data_source/interface/recipe_data_source.dart';
 import 'package:recipe_app/data/data_source/mock/mock_recipe_data_source.dart';
 import 'package:recipe_app/data/repository/interface/recipe_repository.dart';
 import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
-import 'package:recipe_app/presentation/recipe/saved_recipes_view_model.dart';
+import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
 
 void main() {
   final RecipeDataSource dataSource = MockRecipeDataSource();
@@ -13,7 +13,7 @@ void main() {
   test('fetchAll 호출 시 레시피 목록을 가져와야한다.', () async {
     await viewModel.fetchAll();
 
-    expect(viewModel.recipes, isNotEmpty);
-    expect(viewModel.recipes[0].name, 'Traditional spare ribs baked');
+    expect(viewModel.state.recipes, isNotEmpty);
+    expect(viewModel.state.recipes[0].name, 'Traditional spare ribs baked');
   });
 }
