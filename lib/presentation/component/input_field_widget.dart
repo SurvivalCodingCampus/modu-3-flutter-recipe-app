74a2dfa772046ebcab4c7e827728c6b0fe63c4b9
irmu98 <choiroom98@naver.com>
2025-04-14 15:59:40 +0900

    fix: TextField icon추가 및 label 숨기기 가능하게 수정

--- lib/presentation/component/input_field_widget.dart
+++ lib/presentation/component/input_field_widget.dart
@@ -4,38 +4,40 @@ import 'package:recipe_app/ui/text_styles.dart';
 
 class InputFieldWidget extends StatelessWidget {
   final void Function(String value) onValueChange;
-  final String label;
+  final String? label;
+  final Icon? icon;
   final String placeHolder;
-  final String value;
 
   const InputFieldWidget({
     super.key,
-    required this.label,
+    this.label,
+    this.icon,
     required this.placeHolder,
-    required this.value,
     required this.onValueChange,
   });
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController controller = TextEditingController(text: value);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 4.0),
-          child: Text(
-            label,
-            style: TextStyles.smallRegular(color: ColorStyles.labelColor),
-          ),
-        ),
+        label != null
+            ? Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Text(
+                  label!,
+                  style: TextStyles.smallRegular(color: ColorStyles.labelColor),
+                ),
+              )
+            : const SizedBox.shrink(),
         TextField(
           key: const Key('textField'),
-          controller: controller,
           onChanged: (text) => onValueChange(text),
           decoration: InputDecoration(
             hintText: placeHolder,
-            contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            prefixIcon: icon,
+            contentPadding: const EdgeInsets.symmetric(
+                vertical: 12, horizontal: 20),
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
               borderSide: BorderSide(color: ColorStyles.gray4, width: 1.5),
