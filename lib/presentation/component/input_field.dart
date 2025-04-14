c67388e7dcca9d4f0a8d04de272fd4dc4e531105
eastar-DS <sien0104@gmail.com>
2025-04-14 15:20:28 +0900

    refactor: 인풋필드에 컨트롤러 추가

--- lib/presentation/component/input_field.dart
+++ lib/presentation/component/input_field.dart
@@ -2,33 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app/ui/color_style.dart';
 import 'package:recipe_app/ui/text_style.dart';
 
-class InputField extends StatelessWidget {
+class InputField extends StatefulWidget {
   final String label;
   final String placeHolder;
-  final String value;
+  final TextEditingController controller;
   final Function(String) onValueChange;
   const InputField({
     super.key,
     required this.label,
     required this.placeHolder,
-    required this.value,
     required this.onValueChange,
+    required this.controller,
   });
 
   @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  late final TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = widget.controller;
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final bool islabel = widget.label.isNotEmpty;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
       children: [
-        Text(label, style: TextStyles.smallTextRegular),
-        SizedBox(height: 5),
+        if (islabel) Text(widget.label, style: TextStyles.smallTextRegular),
+        if (islabel) SizedBox(height: 5),
         SizedBox(
           height: 55,
           width: double.infinity,
           child: TextFormField(
-            initialValue: value,
-            onChanged: onValueChange,
+            controller: _textEditingController,
+            onChanged: (value) {
+              print(value);
+              widget.onValueChange(value);
+            },
             decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search, color: ColorStyles.gray4),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide: BorderSide(color: ColorStyles.gray4, width: 1.5),
@@ -40,7 +65,7 @@ class InputField extends StatelessWidget {
                   width: 1.5,
                 ),
               ),
-              hintText: placeHolder,
+              hintText: widget.placeHolder,
               hintStyle: TextStyles.smallTextRegular.copyWith(
                 color: ColorStyles.gray4,
               ),
