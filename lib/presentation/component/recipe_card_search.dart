8d1c7066fc79f7ed382d7a569d2d940b47da661a
irmu98 <choiroom98@naver.com>
2025-04-15 01:16:11 +0900

    fix: 누락된 onClick 추가

--- lib/presentation/component/recipe_card_search.dart
+++ lib/presentation/component/recipe_card_search.dart
@@ -17,89 +17,94 @@ class RecipeCardSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: CachedNetworkImage(
-                  imageUrl: recipe.image,
-                  fit: BoxFit.cover,
-                  placeholder:
-                      (context, url) => Center(
-                    child: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: CircularProgressIndicator(),
-                    ),
-                  ),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                ),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: () {
+        onClick();
+      },
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            children: [
+              ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                gradient: LinearGradient(
-                  colors: [Colors.black87, Colors.transparent],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
+                child: SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: CachedNetworkImage(
+                    imageUrl: recipe.image,
+                    fit: BoxFit.cover,
+                    placeholder:
+                        (context, url) => Center(
+                      child: SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  width: 45,
-                  height: 20,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: ColorStyles.secondary20,
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: LinearGradient(
+                    colors: [Colors.black87, Colors.transparent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
                   ),
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.star, size: 11, color: ColorStyles.rating),
-                        Text(
-                          recipe.rating.toStringAsFixed(1),
-                          style: TextStyles.smallerRegular(),
-                        ),
-                      ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 45,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: ColorStyles.secondary20,
+                    ),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.star, size: 11, color: ColorStyles.rating),
+                          Text(
+                            recipe.rating.toStringAsFixed(1),
+                            style: TextStyles.smallerRegular(),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    recipe.name,
-                    style: TextStyles.smallBold(color: ColorStyles.white),
-                  ),
-                  Text(
-                    'By ${recipe.chef}',
-                    style: TextStyles.smallerRegular(
-                      color: ColorStyles.gray4,
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      recipe.name,
+                      style: TextStyles.smallBold(color: ColorStyles.white),
                     ),
-                  ),
-                ],
+                    Text(
+                      'By ${recipe.chef}',
+                      style: TextStyles.smallerRegular(
+                        color: ColorStyles.gray4,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
