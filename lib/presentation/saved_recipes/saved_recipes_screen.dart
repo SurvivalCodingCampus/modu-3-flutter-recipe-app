1a0a58101a0744617b504ef0938fa8632864991f
Junsung Choi <choiexe1@gmail.com>
2025-04-14 13:29:27 +0900

    rename: recipes directory name to saved_recipes

--- lib/presentation/saved_recipes/saved_recipes_screen.dart
+++ lib/presentation/saved_recipes/saved_recipes_screen.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/data/model/recipe.dart';
 import 'package:recipe_app/presentation/component/recipe_card.dart';
-import 'package:recipe_app/presentation/recipe/saved_recipes_view_model.dart';
+import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
 
 class SavedRecipesScreen extends StatelessWidget {
   final SavedRecipesViewModel viewModel;
 
-  SavedRecipesScreen({super.key, required this.viewModel}) {
-    viewModel.fetchAll();
-  }
+  const SavedRecipesScreen({super.key, required this.viewModel});
 
   @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
-      listenable: viewModel,
+      listenable: viewModel..fetchAll(),
       builder: (context, child) {
-        if (viewModel.isLoading) {
+        if (viewModel.state.isLoading) {
           return Scaffold(
             appBar: AppBar(title: const Text('Saved Recipes')),
             body: const Center(child: CircularProgressIndicator()),
@@ -28,9 +26,9 @@ class SavedRecipesScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: ListView.separated(
               separatorBuilder: (context, index) => const SizedBox(height: 8),
-              itemCount: viewModel.recipes.length,
+              itemCount: viewModel.state.recipes.length,
               itemBuilder: (context, index) {
-                final Recipe recipe = viewModel.recipes[index];
+                final Recipe recipe = viewModel.state.recipes[index];
 
                 return RecipeCard(
                   title: recipe.name,
