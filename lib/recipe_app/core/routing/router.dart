37fec7782bd13a58423624eef7c88ff8812ca07c
HyunSoo.Kim <gomtinge326@gmail.com>
2025-04-15 15:42:19 +0900

    feat : route에 SignUp Screen 경로 추가

--- lib/recipe_app/core/routing/router.dart
+++ lib/recipe_app/core/routing/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:recipe_app/recipe_app/core/routing/routes.dart';
 import 'package:recipe_app/recipe_app/presentation/sign_in/sign_in_screen.dart';
+import 'package:recipe_app/recipe_app/presentation/sign_up/sign_up_screen.dart';
 import 'package:recipe_app/recipe_app/presentation/splash_screen/splash_screen.dart';
 
 final router = GoRouter(
@@ -8,5 +9,6 @@ final router = GoRouter(
   routes: [
     GoRoute(path: Routes.splash, builder: (context, state) => SplashScreen()),
     GoRoute(path: Routes.signIn, builder: (context, state) => SignInScreen()),
+    GoRoute(path: Routes.signUp, builder: (context, state) => SignUpScreen()),
   ],
 );
--- lib/recipe_app/core/routing/routes.dart
+++ lib/recipe_app/core/routing/routes.dart
@@ -1,6 +1,7 @@
 abstract class Routes {
   static const String splash = '/splash-screen';
   static const String signIn = '/sign-in-screen';
+  static const String signUp = '/sign-up-screen';
   static const String signOut = '/sign-Out';
   static const String savedRecipes = '/saved-recipes';
   static const String searchRecipes = '/search-recipes';
