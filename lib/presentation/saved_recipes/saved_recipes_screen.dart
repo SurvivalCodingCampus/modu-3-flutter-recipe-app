ea05bc027c1546e9e488fee6118e3c51d8a047ee
Junsung Choi <choiexe1@gmail.com>
2025-04-15 17:42:13 +0900

    refactor: saved recipes screen padding

--- lib/presentation/saved_recipes/saved_recipes_screen.dart
+++ lib/presentation/saved_recipes/saved_recipes_screen.dart
@@ -3,47 +3,56 @@ import 'package:recipe_app/data/model/recipe.dart';
 import 'package:recipe_app/presentation/component/recipe_card.dart';
 import 'package:recipe_app/presentation/saved_recipes/saved_recipes_view_model.dart';
 
-class SavedRecipesScreen extends StatelessWidget {
+class SavedRecipesScreen extends StatefulWidget {
   final SavedRecipesViewModel viewModel;
 
   const SavedRecipesScreen({super.key, required this.viewModel});
 
   @override
+  State<SavedRecipesScreen> createState() => _SavedRecipesScreenState();
+}
+
+class _SavedRecipesScreenState extends State<SavedRecipesScreen> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.fetchAll();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
-      listenable: viewModel..fetchAll(),
+      listenable: widget.viewModel,
       builder: (context, child) {
-        if (viewModel.state.isLoading) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Recipes'),
-              centerTitle: true,
-            ),
-            body: const Center(child: CircularProgressIndicator()),
-          );
-        }
-
         return Scaffold(
           appBar: AppBar(title: const Text('Saved Recipes'), centerTitle: true),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: ListView.separated(
-              separatorBuilder: (context, index) => const SizedBox(height: 8),
-              itemCount: viewModel.state.recipes.length,
-              itemBuilder: (context, index) {
-                final Recipe recipe = viewModel.state.recipes[index];
-
-                return RecipeCard(
-                  title: recipe.name,
-                  cookTime: recipe.cookTime,
-                  imagePath: recipe.imageUrl,
-                  author: recipe.chef,
-                  rating: recipe.rating.toInt(),
-                  onBookmark: () {},
-                );
-              },
-            ),
+            child: buildListView(),
           ),
+        );
+      },
+    );
+  }
+
+  Widget buildListView() {
+    if (widget.viewModel.state.isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return ListView.separated(
+      separatorBuilder: (context, index) => const SizedBox(height: 8),
+      itemCount: widget.viewModel.state.recipes.length,
+      itemBuilder: (context, index) {
+        final Recipe recipe = widget.viewModel.state.recipes[index];
+
+        return RecipeCard(
+          title: recipe.name,
+          cookTime: recipe.cookTime,
+          imagePath: recipe.imageUrl,
+          author: recipe.chef,
+          rating: recipe.rating.toInt(),
+          onBookmark: () {},
         );
       },
     );
