82f50c7bf6f6a02a08ecc71fd995b919dd670553
Junsung Choi <choiexe1@gmail.com>
2025-04-15 17:10:21 +0900

    feat: routes

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:recipe_app/core/routing/routes.dart';
+import 'package:recipe_app/presentation/component/bottom_navigation_bar_scaffold.dart';
 import 'package:recipe_app/presentation/home/home_screen.dart';
 import 'package:recipe_app/presentation/login/login_screen.dart';
 import 'package:recipe_app/presentation/register/register_screen.dart';
 import 'package:recipe_app/presentation/splash/splash_screen.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
 final router = GoRouter(
+  initialLocation: Routes.splash,
+  navigatorKey: _rootNavigatorKey,
   routes: [
     GoRoute(
       path: Routes.splash,
@@ -19,6 +26,20 @@ final router = GoRouter(
       path: Routes.register,
       builder: (context, state) => const RegisterScreen(),
     ),
-    GoRoute(path: Routes.home, builder: (context, state) => const HomeScreen()),
+    StatefulShellRoute.indexedStack(
+      builder:
+          (context, state, child) => BottomNavigationBarScaffold(child: child),
+      branches: [
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorKey,
+          routes: [
+            GoRoute(
+              path: Routes.home,
+              builder: (context, state) => const HomeScreen(),
+            ),
+          ],
+        ),
+      ],
+    ),
   ],
 );
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,6 @@
 abstract class Routes {
-  static const String splash = '/';
-  static const String home = '/home';
+  static const String splash = '/splash';
+  static const String home = '/';
   static const String login = '/login';
   static const String register = '/register';
   static const String recipe = '/recipe';
