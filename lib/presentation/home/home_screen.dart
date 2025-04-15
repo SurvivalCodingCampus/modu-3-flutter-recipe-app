7e8e236a4da76591ca57e16151fbc9897e2bd2b9
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-04-15 09:19:08 +0900

    refactor: ViewModel 의 최초 로딩 시점을 StatefulWidget 의 initState() 로 변경

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -2,19 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app/presentation/component/recipe_widget.dart';
 import 'package:recipe_app/presentation/home/home_view_model.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final HomeViewModel viewModel;
 
   const HomeScreen({super.key, required this.viewModel});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.fetchRecipes();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       body: ListenableBuilder(
-        listenable: viewModel..fetchRecipes(),
+        listenable: widget.viewModel,
         builder: (context, child) {
-          final state = viewModel.state;
+          final state = widget.viewModel.state;
           if (state.isLoading) {
             return CircularProgressIndicator();
           }
