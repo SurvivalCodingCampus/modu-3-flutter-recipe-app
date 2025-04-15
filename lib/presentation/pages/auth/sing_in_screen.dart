fdbb42f89cf61e26de67c02b7f76be8fbd746474
Dansot4891 <cnctlim94@gmail.com>
2025-04-15 13:56:48 +0900

    [feat] 그림자 전용 색상 추가 및 로그인화면 ui 구현 완료

--- lib/presentation/pages/auth/sing_in_screen.dart
+++ lib/presentation/pages/auth/sing_in_screen.dart
@@ -26,14 +26,25 @@ class _SingInScreenState extends State<SingInScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(height: 50),
             Text('Hello', style: AppTextStyle.headerBold),
             Text('Welcome Back!', style: AppTextStyle.largeRegular),
             const SizedBox(height: 50),
             Text('Email', style: AppTextStyle.smallRegular),
-            AppTextField(controller: _email, hintText: 'Enter email'),
+            AppTextField(
+              controller: _email,
+              hintText: 'Enter email',
+              borderColor: AppColor.grey4,
+              textColor: AppColor.grey4,
+            ),
             const SizedBox(height: 30),
             Text('Enter Password', style: AppTextStyle.smallRegular),
-            AppTextField(controller: _email, hintText: 'Enter Password'),
+            AppTextField(
+              controller: _password,
+              hintText: 'Enter Password',
+              borderColor: AppColor.grey4,
+              textColor: AppColor.grey4,
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
               child: Text(
@@ -48,6 +59,83 @@ class _SingInScreenState extends State<SingInScreen> {
               onClick: () {
                 context.replace(AppRoutes.home);
               },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(height: 1, width: 50, color: AppColor.grey4),
+                  Text(
+                    ' Or Sign in With ',
+                    style: AppTextStyle.smallRegular.copyWith(
+                      color: AppColor.grey4,
+                    ),
+                  ),
+                  Container(height: 1, width: 50, color: AppColor.grey4),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: AppColor.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColor.boxShadow.withValues(alpha: 0.15),
+                        spreadRadius: 3,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Image.network(
+                    width: 24,
+                    height: 24,
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png',
+                  ),
+                ),
+                const SizedBox(width: 25),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: AppColor.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColor.boxShadow.withValues(alpha: 0.15),
+                        spreadRadius: 3,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Image.network(
+                    width: 24,
+                    height: 24,
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/640px-Facebook_logo_%28square%29.png',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Don’t have an account?',
+                  style: AppTextStyle.smallerRegular,
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  'Sign up',
+                  style: AppTextStyle.smallerRegular.copyWith(
+                    color: AppColor.secondary100,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
