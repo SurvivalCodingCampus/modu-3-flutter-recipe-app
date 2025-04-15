3c9525b12a62bbbb193a210cf1dcd6d8b0b18a5a
eastar-DS <sien0104@gmail.com>
2025-04-15 14:33:43 +0900

    feat: main에 router추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/test_screen/component_test_screen.dart';
+import 'package:recipe_app/core/routing/router.dart';
 import 'package:recipe_app/ui/color_style.dart';
 
 void main() async {
@@ -13,14 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         scaffoldBackgroundColor: ColorStyles.white,
         // textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: ComponentTestScreen(),
+      routerConfig: router,
     );
   }
 }
