4d2dd24ed2d61ea067bd1bf11dddf78d12f5f94a
Junsung Choi <choiexe1@gmail.com>
2025-04-21 13:09:47 +0900

    feat: error => failure

--- lib/core/result.dart
+++ lib/core/result.dart
@@ -5,5 +5,5 @@ part 'result.freezed.dart';
 @freezed
 sealed class Result<T, E> with _$Result<T, E> {
   const factory Result.success(T data) = Success;
-  const factory Result.error(E error) = Error;
+  const factory Result.failure(E error) = Failure;
 }
