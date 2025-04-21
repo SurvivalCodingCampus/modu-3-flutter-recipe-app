1f2303ea6ad8b0820b7bd6020573a305b36d655c
irmu98 <choiroom98@naver.com>
2025-04-21 14:10:20 +0900

    feat: recentSearch 및 SettingRepository 추가에 따른 router, di 수정

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
 import 'package:recipe_app/data/data_source/chef_data_source.dart';
 import 'package:recipe_app/data/data_source/mock_chef_data_source_impl.dart';
 import 'package:recipe_app/data/data_source/mock_procedure_data_source_impl.dart';
@@ -6,17 +7,23 @@ import 'package:recipe_app/data/data_source/mock_recipe_data_source_impl.dart';
 import 'package:recipe_app/data/data_source/procedure_data_source.dart';
 import 'package:recipe_app/data/data_source/recipe_data_source.dart';
 import 'package:recipe_app/data/repository/chef_repository_impl.dart';
+import 'package:recipe_app/data/repository/mock_recent_search_repository_impl.dart';
+import 'package:recipe_app/data/repository/mock_setting_repository_impl.dart';
 import 'package:recipe_app/data/repository/mock_user_repository_impl.dart';
 import 'package:recipe_app/data/repository/procedure_repository_impl.dart';
 import 'package:recipe_app/data/repository/recipe_repository_impl.dart';
 import 'package:recipe_app/domain/repository/chef_repository.dart';
 import 'package:recipe_app/domain/repository/procedure_repository.dart';
+import 'package:recipe_app/domain/repository/recent_search_repository.dart';
 import 'package:recipe_app/domain/repository/recipe_repository.dart';
+import 'package:recipe_app/domain/repository/setting_repository.dart';
 import 'package:recipe_app/domain/repository/user_repository.dart';
 import 'package:recipe_app/domain/use_case/get_saved_recipes_use_case.dart';
 import 'package:recipe_app/domain/use_case/search_chef_by_recipe_chef_use_case.dart';
+import 'package:recipe_app/domain/use_case/search_recipes_use_case.dart';
 import 'package:recipe_app/domain/use_case/toggle_bookmark_use_case.dart';
 import 'package:recipe_app/presentation/ingredient/search_recipes/search_recipes_view_model.dart';
+import 'package:recipe_app/presentation/ingredient/splash/splash_view_model.dart';
 import 'package:recipe_app/presentation/main/saved_recipes/saved_recipes_view_model.dart';
 
 final getIt = GetIt.instance;
@@ -32,13 +39,17 @@ void diSetup() {
   getIt.registerSingleton<RecipeRepository>(RecipeRepositoryImpl(recipeDataSource: getIt()));
   getIt.registerSingleton<ProcedureRepository>(ProcedureRepositoryImpl(procedureDataSource: getIt()));
   getIt.registerSingleton<ChefRepository>(ChefRepositoryImpl(chefDataSource: getIt()));
-
+  getIt.registerSingleton<SettingRepository>(MockSettingRepositoryImpl(isErrorSituation: true));
+  getIt.registerSingleton<RecentSearchRepository>(MockRecentSearchRepositoryImpl());
+  
   // UseCase
   getIt.registerSingleton(GetSavedRecipesUseCase(userRepository: getIt(), recipeRepository: getIt()));
   getIt.registerSingleton(SearchChefByRecipeChef(chefRepository: getIt(), recipeRepository: getIt()));
   getIt.registerSingleton(ToggleBookmarkUseCase(userRepository: getIt()));
+  getIt.registerSingleton(SearchRecipesUseCase(recentSearchRepository: getIt()));
 
   // ViewModel
   getIt.registerFactory(() => SavedRecipesViewModel(getSavedRecipesUseCase: getIt(), toggleBookmarkUseCase: getIt()));
-  getIt.registerFactory(() => SearchRecipesViewModel(recipeRepository: getIt()));
+  getIt.registerFactory(() => SearchRecipesViewModel(recipeRepository: getIt(), recentSearchRepository: getIt(), searchRecipesUseCase: getIt()));
+  getIt.registerFactory(() => SplashViewModel(settingRepository: getIt()));
 }
