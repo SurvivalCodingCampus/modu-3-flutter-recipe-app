a816e88b67b75255e5a44cbacb86f1a824ba099d
HyunSoo.Kim <gomtinge326@gmail.com>
2025-04-22 09:48:59 +0900

    fix : test코드 로직 수정

--- test/saved_recipes_screen_test.dart
+++ test/saved_recipes_screen_test.dart
@@ -1,21 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe_app/recipe_app/core/di/di_setup.dart';
+import 'package:recipe_app/recipe_app/data/repository/recipe_repository.dart';
+import 'package:recipe_app/recipe_app/data/repository/recipe_repository_impl.dart';
+import 'package:recipe_app/recipe_app/data_source/mock/mock_recipe_data_impl.dart';
+import 'package:recipe_app/recipe_app/presentation/saved_recipes_screen/saved_recipes_screen.dart';
+import 'package:recipe_app/recipe_app/presentation/saved_recipes_screen/saved_recipes_view_model.dart';
 
-
-// void main() {
-//   MockRecipeDataImpl mockRecipeDataImpl = MockRecipeDataImpl();
-//   RecipeRepository repository = RecipeRepositoryImpl(
-//     recipeDataSource: mockRecipeDataImpl,
-//   );
-//   testWidgets('SavedRecipesScreen Check Test', (WidgetTester tester) async {
-//     await tester.pumpWidget(
-//       MaterialApp(
-//         home: Scaffold(
-//           body: SavedRecipesScreen(
-//             savedRecipesViewModel: SavedRecipesViewModel(repository)
-//               ..loadRecipesData(),
-//           ),
-//         ),
-//       ),
-//     );
-//     expect(find.byType(SavedRecipesScreen), findsOneWidget);
-//   });
-// }
+void main() {
+  diSetUp();
+  MockRecipeDataImpl mockRecipeDataImpl = MockRecipeDataImpl();
+  RecipeRepository repository = RecipeRepositoryImpl(
+    recipeDataSource: mockRecipeDataImpl,
+  );
+  testWidgets('SavedRecipesScreen Check Test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: SavedRecipesScreen(
+            savedRecipesViewModel: SavedRecipesViewModel(
+              getIt(),
+              getIt(),
+              getIt(),
+            )..getSavedRecipesUseCase(),
+          ),
+        ),
+      ),
+    );
+    expect(find.byType(SavedRecipesScreen), findsOneWidget);
+  });
+}
