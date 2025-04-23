5cbd5e355de223bfa0f4f6e8ceff1f6cd108bd6a
irmu98 <choiroom98@naver.com>
2025-04-23 17:02:48 +0900

    feat: 페이지 이동을 위한 콜백 onClick 추가

--- lib/presentation/component/recipe_card.dart
+++ lib/presentation/component/recipe_card.dart
@@ -22,36 +22,36 @@ class RecipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 200,
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: SizedBox(
-              width: double.infinity,
-              height: 200,
-              child: CachedNetworkImage(
-                imageUrl: recipe.image,
-                fit: BoxFit.cover,
-                placeholder:
-                    (context, url) => Center(
-                      child: SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: CircularProgressIndicator(),
+    return GestureDetector(
+      onTap: () {
+        onClick(recipe.id);
+      },
+      child: SizedBox(
+        width: double.infinity,
+        height: 200,
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: SizedBox(
+                width: double.infinity,
+                height: 200,
+                child: CachedNetworkImage(
+                  imageUrl: recipe.image,
+                  fit: BoxFit.cover,
+                  placeholder:
+                      (context, url) => Center(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: CircularProgressIndicator(),
+                        ),
                       ),
-                    ),
-                errorWidget: (context, url, error) => Icon(Icons.error),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-              onClick(recipe.id);
-            },
-            child: Container(
+            Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
                 gradient: LinearGradient(
@@ -61,112 +61,112 @@ class RecipeCard extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                width: 45,
-                height: 20,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: ColorStyles.secondary20,
-                ),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.star, size: 11, color: ColorStyles.rating),
-                      Text(
-                        recipe.rating.toStringAsFixed(1),
-                        style: TextStyles.smallerRegular(),
-                      ),
-                    ],
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  width: 45,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: ColorStyles.secondary20,
                   ),
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 3,
-                child: showTitle ?
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        recipe.name,
-                        style: TextStyles.smallBold(color: ColorStyles.white),
-                      ),
-                      Text(
-                        'By ${recipe.chef}',
-                        style: TextStyles.smallerRegular(
-                          color: ColorStyles.gray4,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-                    : Container()
-              ),
-              Expanded(
-                flex: 2,
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
+                  child: Center(
                     child: Row(
-                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Icon(
-                          Icons.timer_sharp,
-                          size: 17,
-                          color: ColorStyles.gray4,
-                        ),
-                        SizedBox(width: 5),
+                        Icon(Icons.star, size: 11, color: ColorStyles.rating),
                         Text(
-                          recipe.time,
-                          style: TextStyles.smallRegular(
-                            color: ColorStyles.gray4,
-                          ),
-                        ),
-                        SizedBox(width: 12),
-                        GestureDetector(
-                          key: const Key('recipeCardBookmarkButton'),
-                          onTap: () {
-                            onBookmark(recipe.id);
-                          },
-                          child: Container(
-                            width: 24,
-                            height: 24,
-                            decoration: BoxDecoration(
-                              color: ColorStyles.white,
-                              shape: BoxShape.circle,
-                            ),
-                            child: Center(
-                              child: Icon(
-                                isBookmarked == true
-                                    ? Icons.bookmark_rounded
-                                    : Icons.bookmark_border_rounded,
-                                size: 15,
-                                color: ColorStyles.primary80,
-                              ),
-                            ),
-                          ),
+                          recipe.rating.toStringAsFixed(1),
+                          style: TextStyles.smallerRegular(),
                         ),
                       ],
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: showTitle ?
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          recipe.name,
+                          style: TextStyles.smallBold(color: ColorStyles.white),
+                        ),
+                        Text(
+                          'By ${recipe.chef}',
+                          style: TextStyles.smallerRegular(
+                            color: ColorStyles.gray4,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                      : Container()
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.timer_sharp,
+                            size: 17,
+                            color: ColorStyles.gray4,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            recipe.time,
+                            style: TextStyles.smallRegular(
+                              color: ColorStyles.gray4,
+                            ),
+                          ),
+                          SizedBox(width: 12),
+                          GestureDetector(
+                            key: const Key('recipeCardBookmarkButton'),
+                            onTap: () {
+                              onBookmark(recipe.id);
+                            },
+                            child: Container(
+                              width: 24,
+                              height: 24,
+                              decoration: BoxDecoration(
+                                color: ColorStyles.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  isBookmarked == true
+                                      ? Icons.bookmark_rounded
+                                      : Icons.bookmark_border_rounded,
+                                  size: 15,
+                                  color: ColorStyles.primary80,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
